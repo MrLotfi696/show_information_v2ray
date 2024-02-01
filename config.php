b652da9cf1d824ac74c6c1fce86aa7cbfb4dbba5
MrLotfi696 <mrlotfi696@gmail.com>
2024-02-01 20:37:02 +0330

    Panel connection code

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+require "./functions/jdf.php";
+require "./functions/XUI.php";
+/* ----- {setting panel xui} ------ */ 
+$XUI = [
+    "url" => "https://Address/",
+    "username" => "username",
+    "password" => "password",
+    "cookie" => "cookie.txt" // do not change
+];
+login();
