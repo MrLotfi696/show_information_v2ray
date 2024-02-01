1ee780c6411cee722e992a68d6e87f60d7772043
MrLotfi696 <mrlotfi696@gmail.com>
2024-02-01 20:19:14 +0330

    Writing html codes

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fa" dir="rtl">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>page one</title>
+    </head>
+
+    <body>
+        <h1>دریافت اطلاعات</h1>
+        <form action="result.php" method="post">
+            <label for="link">لینک اتصال رو وارد کن :</label><br>
+            <input type="text" id="link" name="link" placeholder="لینک اتصال وارد کن ..."><br><br>
+            <input type="submit" value="بررسی">
+        </form>
+    </body>
+
+</html>
