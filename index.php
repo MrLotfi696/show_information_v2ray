f058937c06545da9ffd2173c36a7ca5c929efac5
MrLotfi696 <mrlotfi696@gmail.com>
2024-02-05 00:26:03 +0330

    Add project front

--- index.php
+++ index.php
@@ -4,16 +4,29 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>page one</title>
+        <title>information</title>
+        <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="./assets/css/font-style.css">
     </head>
 
-    <body>
-        <h1>دریافت اطلاعات</h1>
-        <form action="result.php" method="post">
-            <label for="link">لینک اتصال رو وارد کن :</label><br>
-            <input type="text" id="link" name="link" placeholder="لینک اتصال وارد کن ..."><br><br>
-            <input type="submit" value="بررسی">
-        </form>
+    <body class="container">
+
+        <div class="position-absolute top-50 start-50 translate-middle w-75 rounded p-5 shadow-sm bg-box">
+            <h1 class="text-center mb-4 font-yekan-ExtraBold">نمایش اطلاعات سرویس</h1>
+            <hr>
+            <form action="result.php" method="post" class="mt-4 mb-2">
+                <div class="mb-3">
+                    <label for="input_link" class="form-label color-text fs-5 font-yekan-bold font-size-label">لینک را وارد کنید</label>
+                    <input type="text" class="form-control" id="input_link" name="input_link" aria-describedby="input_link_des">
+                    <div id="input_link_des" class="form-text font-yekan-reqular font-size-help">لینک را به درستی وارد کنید.</div>
+                </div>
+                <button type="submit" class="btn btn-primary font-yekan-bold font-size-button">بررسی</button>
+            </form>
+            
+        </div>
+        
+        <script src="./assets/js/bootstrap.bundle.min.js"></script>
     </body>
 
 </html>
--- result.php
+++ result.php
@@ -1,8 +1,8 @@
 <?php
 require "config.php";
 
-if (isset($_POST['link']) and !empty($_POST['link'])) {
-    $Get_uuid = GetUUID($_POST['link']);
+if (isset($_POST['input_link']) and !empty($_POST['input_link'])) {
+    $Get_uuid = GetUUID($_POST['input_link']);
     if ($Get_uuid['success'] == true) {
         $result = getInfo($Get_uuid['result']);
         if ($result['msg'] !== "ok") {
@@ -16,41 +16,76 @@ if (isset($_POST['link']) and !empty($_POST['link'])) {
 
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
+
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>page two</title>
-        <style>
-            table, th, td {
-                border:1px solid black;
-            }
-        </style>
+        <title>information</title>
+        <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
+        <link rel="stylesheet" href="./assets/css/style.css">
+        <link rel="stylesheet" href="./assets/css/font-style.css">
     </head>
-    <body>
-        <h1>نمایش اطلاعات</h1>
-        <table>
-            <tr>
-                <th>نام</th>
-                <th>ایمیل</th>
-                <th>حجم دانلود</th>
-                <th>حجم آپلود</th>
-                <th>حجم کل</th>
-                <th>زمان پایان</th>
-                <th>ترافیک باقی مانده</th>
-                <th>زمان باقی مانده</th>
-                <th>حجم کل مصرف</th>
-            </tr>
-            <tr>
-                <td><?php echo $result['name']; ?></td>
-                <td><?php echo $result['email']; ?></td>
-                <td><?php echo $result['down']; ?></td>
-                <td><?php echo $result['up']; ?></td>
-                <td><?php echo $result['total']; ?></td>
-                <td><?php echo $result['expiryTime']; ?></td>
-                <td><?php echo $result['remaining_trafic']; ?></td>
-                <td><?php echo $result['remaining_days']; ?></td>
-                <td><?php echo $result['using_all']; ?></td>
-            </tr>
-        </table>
+
+    <body class="container mb-5 mt-5">
+
+        <div class="box-info position-absolute top-50 start-50 translate-middle w-75 rounded shadow-sm bg-box">
+
+            <h1 class="text-center mb-4 font-yekan-ExtraBold">نمایش اطلاعات سرویس</h1>
+            <hr>
+
+            <div class="boxs-inforamtion p-2">
+
+                <div class="box-one-inforamtion border w-100 rounded shadow-sm col">
+
+                    <div class="d-flex justify-content-between mt-3 p-3">
+
+                        <div class="key">
+                            <p>نام سرویس</p>
+                            <p>وضعیت سرویس</p>
+                            <p>پروتکل سرویس</p>
+                            <p>تاریخ انقضا</p>
+                            <p>حجم کل</p>
+                        </div>
+                        <div class="value">
+                            <p><?php echo $result['name']; ?></p>
+                            <p><?php echo $result['status']; ?></p>
+                            <p><?php echo $result['protocol']; ?></p>
+                            <p><?php echo $result['expiryTime']; ?></p>
+                            <p><?php echo $result['total']; ?></p>
+                        </div>
+                        
+                    </div>
+                
+                </div>
+                
+                <div class="box-two-inforamtion border w-100 rounded shadow-sm col">
+                    
+                    <div class="d-flex justify-content-between mt-3 p-3">
+
+                        <div class="key">
+                            <p>حجم دانلود</p>
+                            <p>حجم آپلود</p>
+                            <p>حجم کل مصرف</p>
+                            <p>ترافیک باقی مانده</p>
+                            <p>زمان باقی مانده (روز)</p>
+                        </div>
+                        <div class="value">
+                            <p><?php echo $result['down']; ?></p>
+                            <p><?php echo $result['up']; ?></p>
+                            <p><?php echo $result['using_all']; ?></p>
+                            <p><?php echo $result['remaining_trafic']; ?></p>
+                            <p><?php echo $result['remaining_days']; ?></p>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+        <script src="./assets/js/bootstrap.bundle.min.js"></script>
     </body>
+
 </html>
